83610de7e0392dbb84bac39fd0c4c19ff8508bb7
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-03-15 10:16:45 +0200

    added

--- Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main (1)/Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
+++ Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main (1)/Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
@@ -8,7 +8,7 @@ function validateSyntax() {
     let resultElement = document.getElementById("result"); // Placeholder for validation result
 
     if (isValid) {
-        resultElement.innerHTML = "Valid Syntax. 🟢" 
+        resultElement.innerHTML = "Valid Syntax. 🟢" // I used emojipedia for my red and green circles, I just copied the emoji and paste it in my code
 
     } else {
         resultElement.innerHTML = "Invalid Syntax. 🔴"
